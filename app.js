e2159fe3872b94942a3c9c03c982d2b0577ee475
yuki <y_manome@aaa.a>
2017-10-04 10:36:24 +0000

    ランキングの実装完了

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 'use strict';
+const fs = require('fs');
+const readline = require('readline');
+const rs = fs.ReadStream('./popu-pref.csv');
+const rl = readline.createInterface({ 'input': rs, 'output': {} });
+const map = new Map(); // key: 都道府県 value: 集計データのオブジェクト
+rl.on('close', () => {
+    for (let pair of map) {
+        const value = pair[1];
+        value.change = value.popu15 / value.popu10;
+    }
+    const rankingArray = Array.from(map).sort((pair1, pair2) => {
+        return pair2[1].change - pair1[1].change;
+    });
+    const rankingStrings = rankingArray.map((pair) => {
+        return pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
+    });
+    console.log(rankingStrings);
+});
+rl.resume();
+rl.on('close', () => {
+    console.log(map);
+});
